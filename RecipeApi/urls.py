141bb92463411ac2595adf0d6f3e04417201d835
Ksenia Mayakovskaya <kseniadmay@gmail.com>
2026-03-08 17:49:14 +0300

    Добавила главный urls

--- RecipeApi/urls.py
+++ RecipeApi/urls.py
@@ -14,9 +14,42 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+# Swagger документация
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Recipe API',
+        default_version='v1',
+        description='API для управления рецептами',
+        contact=openapi.Contact(email='kseniadmay@gmail.com'),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # API
+    path('api/', include('recipes.urls')),
+
+    # JWT Аутентификация
+    path('api/auth/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Swagger документация
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+# Для разработки раздача медиа файлов
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
